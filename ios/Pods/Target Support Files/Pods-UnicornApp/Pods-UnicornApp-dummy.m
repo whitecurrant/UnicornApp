bfa91a5d6f37a0f55bae2bd8f3f43f7096f55307
Marcin <mcn.adamczyk@gmail.com>
2019-07-13 11:43:38 +0200

    Project setup

---
+++ ios/Pods/Target Support Files/Pods-UnicornApp/Pods-UnicornApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnicornApp : NSObject
+@end
+@implementation PodsDummy_Pods_UnicornApp
+@end
---
+++ ios/Pods/Target Support Files/Pods-UnicornAppTests/Pods-UnicornAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnicornAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_UnicornAppTests
+@end
